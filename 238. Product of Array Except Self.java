14ca1bd6f2675b8b8d34acd4a11093234216286d
Ananyasree S <68598050+Ananyasree-S@users.noreply.github.com>
2025-07-23 21:37:42 +0530

    Create 238. Product of Array Except Self.java

---
+++ 238. Product of Array Except Self.java
@@ -0,0 +1,26 @@
+/**
+238. Product of Array Except Self
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+**/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        
+        //calc left product
+        res[0] = 1; 
+        for(int i=1;i<nums.length;i++){
+            res[i] = res[i-1] * nums[i-1];
+        }
+
+        int rightpro = 1;
+        for(int i= nums.length-1;i>=0;i--){
+            res[i] *= rightpro;
+            rightpro *= nums[i];
+        }
+        return res;
+    }
+}
