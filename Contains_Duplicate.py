f918d3a41eef061100e9d6a335d499256ee9e1c0
Ananyasree S <68598050+Ananyasree-S@users.noreply.github.com>
2026-02-02 18:51:15 +0530

    Create Contains_Duplicate.py

---
+++ Contains_Duplicate.py
@@ -0,0 +1,17 @@
+''' - Ques
+Contains Duplicate
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct'''
+
+''' - Algo
+Create a set,
+iterate through numdss
+if the ele in set return true
+else add it to set '''
+
+def Containsdup():
+  hs = set()
+  for n in nums:
+    if n in hs:
+      return True
+    hs.add(n)
+  return False
